bec50c110fe1411a3b1e0368961fb2368431b4b2
Vineet Dwivedi <vineetdwivedi409@gmail.com>
2026-03-31 18:05:07 +0530

    Backend deployment

--- Backend/src/sockets/server.socket.js
+++ Backend/src/sockets/server.socket.js
@@ -1,10 +1,26 @@
 import { Server } from "socket.io";
 
 let io;
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174"
+].filter(Boolean);
+
 export function initSocket(httpServer){
     io = new Server(httpServer,{
         cors:{
-            origin: "http://localhost:5173",
+            origin(origin, callback) {
+                if (!origin || allowedOrigins.includes(origin)) {
+                    return callback(null, true);
+                }
+
+                return callback(new Error(`Socket.IO CORS blocked for origin: ${origin}`));
+            },
             credentials: true,
         }
     })
